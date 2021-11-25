e3077e2e55a0814ad88e241793b6c5b42e32927f
shakedx <kolesnikov_danil123@mail.ru>
2021-11-25 15:51:29 +0200

    Добавьте файлы проекта.

---
+++ 3.1_normal_lvl/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Kolesnikov_3._1_Normal_lvl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите k:");
+                int k = int.Parse(Console.ReadLine());
+                double A = 0;
+                double P = 1;
+                for (int j = 1; j <= k; j++)
+                {
+                    {
+                        if (j == 4) continue;
+                        P *= ((j * j) - 4) * j / (j - k + 1);
+                    }
+                    double S = 0;
+                    for (int i = 1; i <= 9; i++)
+                    {
+                        S = (i - 3) / (i - 7);
+                    }
+                    Console.WriteLine($"A={P * S:F2}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
